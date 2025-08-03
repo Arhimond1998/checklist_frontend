abb4792676b13e0655ccd1dfb0d47af138a08bda
dokhuzhev_ei <dokhuzhev_ei@magnit.ru>
2025-08-04 02:29:56 +0300

    new pics for test

--- src/routes/(authed)/checklist/[slug]/+page.js
+++ src/routes/(authed)/checklist/[slug]/+page.js
@@ -5,7 +5,7 @@ export async function load(params) {
         id: 1,
         description: 'Первое задание',
         files: [
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw'
+          'https://opis-cdn.tinkoffjournal.ru/mercury/z99km5RCsG.in-out-shrek-5-preview.jpg?preset=image_1280w_2x'
         ],
         text: 'Первое задание'
       },
@@ -13,15 +13,13 @@ export async function load(params) {
         id: 2,
         description: 'Второе задание',
         files: [
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
+          'https://ss.metronews.ru/userfiles/materials/199/1991989/858x540.jpg',
+          'https://play-lh.googleusercontent.com/_ji2P0ZrM4wAIMvKn9Agz7u1ViCx9gBs8axFNTUSgQqPkl2WpjgzjbhhZtI6D0b6KCKImQ=w240-h480-rw',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Happy_Shrek_%28cropped%29.jpg/330px-Happy_Shrek_%28cropped%29.jpg',
+          'https://wikimultia.org/images/thumb/3/39/Shrek.jpg/350px-Shrek.jpg',
+          'https://sun1-20.userapi.com/impg/2kPKoOGLIYVeeRD5htJnzunUHWdgDvvYeHFu1Q/0DoADpXJf9Y.jpg?size=1280x720&quality=95&sign=f47c0cbf85d79bc08ea32b8ea4590f68&type=video_thumb',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIEmJR9UtJ8KnwULpf-1KF368HWodibT7zfw&s',
+          'https://multoigri.ru/images/wiki/icon-list/shrek.jpg',
           'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw',
         ],
         text: 'Второе задание'
@@ -30,7 +28,7 @@ export async function load(params) {
         id: 3,
         description: 'qwe',
         files: [
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw'
+          'https://img5tv.cdnvideo.ru/webp/shared/files/202110/1_1415459.jpg'
         ],
         text: 'asd'
       },
@@ -38,7 +36,7 @@ export async function load(params) {
         id: 4,
         description: 'qwe',
         files: [
-          'https://play-lh.googleusercontent.com/xPzgMarIJ4s9baKA6HpYA-en_ir4mUToYcDtJLQXPOEPrTXLp7_Wjmjdj5oTcL_nzw'
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Happy_Shrek_%28cropped%29.jpg/330px-Happy_Shrek_%28cropped%29.jpg'
         ],
         text: 'asd'
       }
