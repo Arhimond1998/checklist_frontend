563bbd2e4fe45a7a778241467aa3bcdd417a3383
dokhuzhev_ei <dokhuzhev_ei@magnit.ru>
2025-09-14 21:58:01 +0300

    увеличение ширины

--- src/routes/(authed)/report/+page.svelte
+++ src/routes/(authed)/report/+page.svelte
@@ -49,44 +49,44 @@
 </script>
 
 <Filters bind:isDialogOpen bind:filters></Filters>
-<div class="mt-8 grid gap-1 grid-cols-{colsNum} max-w-50">
+<div class="mt-8 grid gap-1 grid-cols-{colsNum} max-w-500">
   <Button onclick={() => (isDialogOpen = true)}>Фильтры</Button>
   <Button onclick={onFindClick}>Найти</Button>
 
-{#if items.length > 0}
-  <div class="mt-8 grid gap-8 grid-cols-{colsNum} max-w-500">
-    {#each items as checklistItem (checklistItem.id_checklist_user_report)}
-      <Card.Root
-        class="w-full max-w-sm {calculateColor(checklistItem.score, checklistItem.max_score)}"
-      >
-        <Card.Header>
-          <Card.Title>Чеклист: {checklistItem.title}</Card.Title>
-        </Card.Header>
-        <Card.Content>
-          <div>ФИО: {checklistItem.user_fullname}</div>
-          <div>Время заполнения: {checklistItem.dt}</div>
-          <div>Магазин: {checklistItem.name_store} ({checklistItem.code_store})</div>
-          <div>Работник: {checklistItem.employee_fullname}</div>
-        </Card.Content>
-        <Card.Footer class="flex-col gap-2">
-          <div>
-            {Math.floor((checklistItem.score / Math.max(checklistItem.max_score, 1)) * 100 * 100) /
-              100}%
-          </div>
-          <Button onclick={onclick(checklistItem.id_checklist_user_report)} class="w-full"
-            >Посмотреть</Button
-          >
-        </Card.Footer>
-      </Card.Root>
-    {/each}
-  </div>
-{:else}
-  <label for="return_btn">Чеклистов нет.</label>
-  <Button
-    id="return_btn"
-    onclick={() => {
-      goto('/');
-    }}>На главную страницу</Button
-  >
-{/if}
+  {#if items.length > 0}
+    <div class="mt-8 grid gap-8 grid-cols-{colsNum} max-w-500">
+      {#each items as checklistItem (checklistItem.id_checklist_user_report)}
+        <Card.Root
+          class="w-full max-w-sm {calculateColor(checklistItem.score, checklistItem.max_score)}"
+        >
+          <Card.Header>
+            <Card.Title>Чеклист: {checklistItem.title}</Card.Title>
+          </Card.Header>
+          <Card.Content>
+            <div>ФИО: {checklistItem.user_fullname}</div>
+            <div>Время заполнения: {checklistItem.dt}</div>
+            <div>Магазин: {checklistItem.name_store} ({checklistItem.code_store})</div>
+            <div>Работник: {checklistItem.employee_fullname}</div>
+          </Card.Content>
+          <Card.Footer class="flex-col gap-2">
+            <div>
+              {Math.floor((checklistItem.score / Math.max(checklistItem.max_score, 1)) * 100 * 100) /
+                100}%
+            </div>
+            <Button onclick={onclick(checklistItem.id_checklist_user_report)} class="w-full"
+              >Посмотреть</Button
+            >
+          </Card.Footer>
+        </Card.Root>
+      {/each}
+    </div>
+  {:else}
+    <label for="return_btn">Чеклистов нет.</label>
+    <Button
+      id="return_btn"
+      onclick={() => {
+        goto('/');
+      }}>На главную страницу</Button
+    >
+  {/if}
 </div>
